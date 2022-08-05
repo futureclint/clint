f066c32015b84f9cc4740718b5cc0aadb6557958
Clint Gunter <clint@clint.io>
2022-08-05 16:55:05 -0500

    Fixed instances of using class instead of className

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './css/app.css';
 function App() {
   return (
     <div className="app-wrapper">
-      <div class="visually-hidden">
+      <div className="visually-hidden">
         <h1>Clint Gunter</h1>
       </div>
       <header>
-        <img class="logo" src={logo} alt="Clint Gunter" />
+        <img className="logo" src={logo} alt="Clint Gunter" />
         <p>Hi. I'm Clint Gunter, software engineer who loves design. My favorite work is where programming meets design.</p>
       </header>
       <hr />
       <main>
-        <section class="about">
+        <section className="about">
           <p>This is the about section with link to resume and photo.</p>
         </section>
         <hr />
-        <section class="projects">
+        <section className="projects">
           <p>This is the section highlighting projects.</p>
         </section>
         <hr />
-        <section class="contact">
+        <section className="contact">
           <p>This is the contact me section.</p>
         </section>
       </main>
