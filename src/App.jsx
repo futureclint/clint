8097f0709d2295ce9fc24a8d46e24e690f0948e6
Clint Gunter <clint@clint.io>
2022-08-20 17:55:04 -0500

    Added basic text styles

--- src/App.jsx
+++ src/App.jsx
@@ -67,14 +67,14 @@ function App() {
         {/* Projects Section */}
         <section className="projects">
           <h2>Projects</h2>
-          <section>
+          <div className="project">
             <h3>When to Go Outside</h3>
             <p>Ever wondered when it would be best to go outside for a bit? This simple website uses your geolocation to pull weather data from the OpenWeather API, then calculates the span(s) of time it would be best to go outside. This project is a work in progress.</p>
-          </section>
-          <section>
+          </div>
+          <div className="project">
             <h3>Inflation Station</h3>
             <p>Inflation calculators are mostly terrible, so I thought it would be fun to design my own. Drawing from a database of CPI date, it calculates the relative value of items, accounting for inflation. This project is a work in progress.</p>
-          </section>
+          </div>
         </section>
 
         {/* Contact Section */}
