d2cb9a7d6ff44dd9ecfe362a349fede7399fb4f2
Clint Gunter <clint@clint.io>
2022-08-24 16:55:51 -0500

    Added GitHub and LinkedIn URLs

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
         <section className="contact">
           <h2>Contact</h2>
           <p>I'd love to hear from you! The easiest way to contact me is to <a href="mailto:&#99;&#108;&#105;&#110;&#116;&#64;&#99;&#108;&#105;&#110;&#116;&#46;&#105;&#111;?subject=Let’s Work Together" target="_blank" rel="noreferrer">send me an email</a>. <i>Please note, I cannot respond to all email messages. I will respond to messages that are a good fit for my schedule and interests.</i></p>
-          <p>You can also <Link to="/files/clint-gunter-resume.pdf" target="_blank" download>download my resume</Link>, visit me on <a href="#" target="_blank" rel="noopener">GitHub</a>, or visit me on <a href="#" target="_blank" rel="noopener">LinkedIn</a>.</p>
+          <p>You can also <Link to="/files/clint-gunter-resume.pdf" target="_blank" download>download my resume</Link>, visit me on <a href="https://github.com/futureclint" target="_blank" rel="noreferrer">GitHub</a>, or visit me on <a href="https://www.linkedin.com/in/futureclint/" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
         </section>
 
       </main>
