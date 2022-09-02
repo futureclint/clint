081bbd8b0c86281348696f9c3cf87d12b3c9d959
Clint Gunter <clint@clint.io>
2022-09-01 19:01:27 -0500

    Added CSS grid layout and enhanced the layout for header/logo

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,15 @@ function App() {
         <header>
           <div className="logo">
             <div className="logo-lines">
-              <div className="logo-line-shape" id="line-1"></div>
-              <div className="logo-line-shape" id="line-2"></div>
-              <div className="logo-line-shape" id="line-3"></div>
-              <div className="logo-line-shape" id="line-4"></div>
-              <div className="logo-line-shape" id="line-5"></div>
-              <div className="logo-line-shape" id="line-6"></div>
-              <div className="logo-line-shape" id="line-7"></div>
+              <div className="logo-lines-shape-wrapper">
+                <div className="logo-line-shape" id="line-1"></div>
+                <div className="logo-line-shape" id="line-2"></div>
+                <div className="logo-line-shape" id="line-3"></div>
+                <div className="logo-line-shape" id="line-4"></div>
+                <div className="logo-line-shape" id="line-5"></div>
+                <div className="logo-line-shape" id="line-6"></div>
+                <div className="logo-line-shape" id="line-7"></div>
+              </div>
             </div>
             <img className="logo-text" src={logo} alt="Clint Gunter" draggable="false" />
           </div>
