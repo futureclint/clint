1edb7df39c8382f4419a684e8269d16880bc18ee
Clint Gunter <clint@clint.io>
2022-09-06 17:59:53 -0500

    Added explicit colors for each skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,24 +3,24 @@ function Skills() {
     <section className="skills">
       <h2>Skills</h2>
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Sass</li>
-        <li>JavaScript</li>
-        <li>Ruby</li>
-        <li>React</li>
-        <li>Aurelia</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MongoDB</li>
-        <li>Ruby on Rails</li>
-        <li>PostgreSQL</li>
-        <li>Git</li>
-        <li>UI/UX Design</li>
-        <li>Branding</li>
-        <li>Liquid</li>
-        <li>Shopify</li>
-        <li>Statamic</li>
+        <li className="skill-html">HTML</li>
+        <li className="skill-css">CSS</li>
+        <li className="skill-sass">Sass</li>
+        <li className="skill-javascript">JavaScript</li>
+        <li className="skill-ruby">Ruby</li>
+        <li className="skill-react">React</li>
+        <li className="skill-aurelia">Aurelia</li>
+        <li className="skill-node">Node</li>
+        <li className="skill-express">Express</li>
+        <li className="skill-mongodb">MongoDB</li>
+        <li className="skill-rubyonrails">Ruby on Rails</li>
+        <li className="skill-postgresql">PostgreSQL</li>
+        <li className="skill-git">Git</li>
+        <li className="skill-uiuxdesign">UI/UX Design</li>
+        <li className="skill-branding">Branding</li>
+        <li className="skill-liquid">Liquid</li>
+        <li className="skill-shopify">Shopify</li>
+        <li className="skill-statamic">Statamic</li>
       </ul>
     </section>
   );
