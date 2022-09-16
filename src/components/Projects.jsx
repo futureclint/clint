8c9dffcab5702e5738c5a96412f6efc51b651ffa
Clint Gunter <clint@clint.io>
2022-09-16 17:11:34 -0500

    Adjusted layout for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,10 +5,12 @@ function Projects() {
       <div className="project">
         <h3>When to Go Outside</h3>
         <p>Ever wondered when it would be best to go outside for a bit? This simple website uses your geolocation to pull weather data from the OpenWeather API, then calculates the span(s) of time it would be best to go outside. This project is a work in progress.</p>
+        <p><a href="https://github.com/futureclint/when-to-go-outside" target="_blank" rel="noopener noreferrer">View Project <span className="icon-arrow">&#8599;</span></a></p>
       </div>
       <div className="project">
         <h3>Inflation Station</h3>
         <p>Inflation calculators are mostly terrible, so I thought it would be fun to design my own. Drawing from a database of CPI date, it calculates the relative value of items, accounting for inflation. This project is a work in progress.</p>
+        <p><a href="https://github.com/futureclint/inflation-station" target="_blank" rel="noopener noreferrer">View Project <span className="icon-arrow">&#8599;</span></a></p>
       </div>
     </section>
   );
