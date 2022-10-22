f09bcebc174a6577ea90a6da18311052216735f3
Clint Gunter <clint@clint.io>
2022-10-22 13:25:08 -0500

    Aligned component imports for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Header from './components/Header.jsx';
-import About from './components/About.jsx';
-import Skills from './components/Skills.jsx';
+import Header   from './components/Header.jsx';
+import About    from './components/About.jsx';
+import Skills   from './components/Skills.jsx';
 import Projects from './components/Projects.jsx';
-import Contact from './components/Contact.jsx';
-import Footer from './components/Footer.jsx';
+import Contact  from './components/Contact.jsx';
+import Footer   from './components/Footer.jsx';
 import './css/app.css';
 
 function App() {
