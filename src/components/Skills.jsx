e712cec3acd74ea8657dc530338e0e4d29488f9f
Clint Gunter <clint@clint.io>
2023-02-02 16:08:50 -0600

    Revised skills list to match updated resume

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,16 +11,20 @@ function Skills() {
         <li className="skill-react">React</li>
         <li className="skill-aurelia">Aurelia</li>
         <li className="skill-node">Node</li>
-        <li className="skill-express">Express</li>
-        <li className="skill-mongodb">MongoDB</li>
         <li className="skill-rubyonrails">Ruby on Rails</li>
         <li className="skill-postgresql">PostgreSQL</li>
+        <li className="skill-mongodb">MongoDB</li>
         <li className="skill-git">Git</li>
-        <li className="skill-uiuxdesign">UI/UX Design</li>
-        <li className="skill-branding">Branding</li>
         <li className="skill-liquid">Liquid</li>
         <li className="skill-shopify">Shopify</li>
         <li className="skill-statamic">Statamic</li>
+        <li className="skill-wordpress">WordPress</li>
+        <li className="skill-drupal">Drupal</li>
+        <li className="skill-uiuxdesign">UI & UX Design</li>
+        <li className="skill-sketch">Sketch</li>
+        <li className="skill-figma">Figma</li>
+        <li className="skill-photoshop">Photoshop</li>
+        <li className="skill-illustrator">Illustrator</li>
       </ul>
     </section>
   );
